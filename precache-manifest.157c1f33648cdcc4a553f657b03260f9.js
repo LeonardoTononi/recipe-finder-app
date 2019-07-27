8bb43eaa8d602d9dc6eea1644096116bb7b68b77
Leonardo Tononi <easaaa@protonmail.com>
2019-07-27 19:51:14 +0200

    Updates

---
+++ precache-manifest.157c1f33648cdcc4a553f657b03260f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf41622be1630426c75888ed0f66eede",
+    "url": "/Easaaa/recipe-finder-app/index.html"
+  },
+  {
+    "revision": "893eb85722c1da695a56",
+    "url": "/Easaaa/recipe-finder-app/static/css/main.dd723627.chunk.css"
+  },
+  {
+    "revision": "1e19189615b56bb9fceb",
+    "url": "/Easaaa/recipe-finder-app/static/js/2.718419cc.chunk.js"
+  },
+  {
+    "revision": "893eb85722c1da695a56",
+    "url": "/Easaaa/recipe-finder-app/static/js/main.45cdb258.chunk.js"
+  },
+  {
+    "revision": "94b94bc7a4bb0c158d02",
+    "url": "/Easaaa/recipe-finder-app/static/js/runtime~main.f3b5215d.js"
+  }
+]);
